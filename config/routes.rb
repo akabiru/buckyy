63e9cd556880c46371ed096e31a170e90aad4760
Austin Kabiru <austin.kabiru@andela.com>
2016-06-21 09:49:08 +0300

    feat(routes): add api version constraint
     - add heroku procfile and puma config
    
    [Finishes #121737791]

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, path: '/', defaults: { format: 'json' } do
-    namespace :v1 do
-      with_options except: [:new] do |list_only|
+    scope module: :v1, constraints: ApiVersion.new('v1', true) do
+      with_options except: [:new, :edit] do |list_only|
         list_only.resources :bucket_lists do
           list_only.resources :items
         end
